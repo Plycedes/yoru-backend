13b993d65c5c8564dd764d1a85ca58468e5cea4b
Plycedes <abhaypratapyadav340@gmail.com>
2025-01-17 21:59:19 +0530

    Create like

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -5,5 +5,33 @@ import { ApiError } from "../utils/ApiError.js";
 import { Like } from "../models/like.model.js";
 
 export const createLike = asyncHandler(async (req, res) => {
-  return res.send({ status: "OK" });
+  const { videoId, userId, creatorId } = req.body;
+
+  if (!videoId || !userId || !creatorId) {
+    throw new ApiError(400, "No field can be empty");
+  }
+
+  const alreadyLiked = await Like.findOne({
+    $and: [{ videoId: videoId }, { userId: userId }],
+  });
+
+  if (alreadyLiked) {
+    throw new ApiError(409, "Video already bookmarked");
+  }
+
+  const like = await Like.create({
+    videoId,
+    userId,
+    creatorId,
+  });
+
+  const likeCreated = await Like.findById({ _id: like._id });
+
+  if (!likeCreated) {
+    throw new ApiError(500, "Error adding the bookmark");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, likeCreated, "Bookmark created successfully"));
 });
--- src/models/like.model.js
+++ src/models/like.model.js
@@ -5,14 +5,17 @@ const likeSchema = new Schema(
     videoId: {
       type: Schema.Types.ObjectId,
       ref: "Video",
+      required: true,
     },
-    userID: {
+    userId: {
       type: Schema.Types.ObjectId,
       ref: "User",
+      required: true,
     },
     creatorId: {
       type: Schema.Types.ObjectId,
       ref: "User",
+      required: true,
     },
   },
   { timestamps: true }
