fc073271dcbfcf7bbdca739d16bdb5dbee5112ee
Plycedes <abhaypratapyadav340@gmail.com>
2025-01-21 12:08:49 +0530

    Ensure directory

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
+import fs from "fs";
+import path from "path";
 
 dotenv.config({ path: "./env" });
 
@@ -9,6 +11,11 @@ connectDB()
     app.listen(process.env.PORT || 8000, () => {
       console.log(`Server Running at PORT: ${process.env.PORT}`);
     });
+    const tempDir = path.join("./files/temp");
+    if (!fs.existsSync(tempDir)) {
+      fs.mkdirSync(tempDir, { recursive: true });
+      console.log("Folder created");
+    }
   })
   .catch((err) => {
     console.log("Failed to start server!!", err);
