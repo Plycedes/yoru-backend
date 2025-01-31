63ea51d5704d0bb6ed4ae3a0c53e23e9700bd1de
Plycedes <abhaypratapyadav340@gmail.com>
2025-01-31 11:59:29 +0530

    Revert "Using https"
    This reverts commit 14f224362b890488a097ea15fb6b7f4422b78a0f.

--- src/index.js
+++ src/index.js
@@ -3,31 +3,19 @@ import connectDB from "./db/index.js";
 import { app } from "./app.js";
 import fs from "fs";
 import path from "path";
-import https from "https";
 
 dotenv.config({ path: "./env" });
 
-const SSL_KEY_PATH = path.join("..", "certs", "server.key");  // Private Key
-const SSL_CERT_PATH = path.join("..", "certs", "server.crt");  // Certificate
-
 connectDB()
   .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server Running at PORT: ${process.env.PORT}`);
+    });
     const tempDir = path.join("./temp/files");
     if (!fs.existsSync(tempDir)) {
       fs.mkdirSync(tempDir, { recursive: true });
       console.log("Folder created");
     }
-
-    // Read SSL Certificates
-    const options = {
-      key: fs.readFileSync(SSL_KEY_PATH),
-      cert: fs.readFileSync(SSL_CERT_PATH),
-    };
-
-    // Start HTTPS Server
-    https.createServer(options, app).listen(process.env.PORT || 8000, () => {
-      console.log(`HTTPS Server Running at PORT: ${process.env.PORT || 8000}`);
-    });
   })
   .catch((err) => {
     console.log("Failed to start server!!", err);
