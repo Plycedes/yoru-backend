52d0af925818dc4f0ea6d038b589aba0d620047f
Plycedes <abhaypratapyadav340@gmail.com>
2025-02-02 22:20:26 +0530

    Register User

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -9,6 +9,37 @@ import { MulterRequest } from "../middlewares/multer.middleware";
 import { generateProfilePicture } from "../utils/generateProfilePicture";
 import { uploadOnCloudinary, deleteFromCloudinary } from "../utils/cloudinary.js";
 
-export const registerUser = asyncHandler(async (req: Request, res: Response) => {
-    res.send({ status: "OK" });
+export const registerUser = asyncHandler(async (req: Request, res: Response): Promise<Response> => {
+    const { email, username, password } = req.body;
+
+    if ([email, username, password].some((field) => field?.trim() === "")) {
+        throw new ApiError(400, "No field can be empty");
+    }
+
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+
+    if (existingUser) {
+        throw new ApiError(409, "Username or Email already exists");
+    }
+
+    const pfp = await generateProfilePicture(username);
+    if (!pfp) {
+        throw new ApiError(500, "Failed to generate profile picture");
+    }
+
+    const user = await User.create({
+        username,
+        email,
+        password,
+        avatar: pfp.url,
+        avatarId: pfp.public_id,
+    });
+
+    const createdUser = await User.findById(user._id).select("-password -refreshToken");
+
+    if (!createdUser) {
+        throw new ApiError(500, "Error while creating new user");
+    }
+
+    return res.status(200).json(new ApiResponse(200, createdUser, "User created successfully"));
 });
--- src/utils/asyncHandler.ts
+++ src/utils/asyncHandler.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 
-const asyncHandler = (
-    requestHandler: (req: Request, res: Response, next: NextFunction) => Promise<any>
+const asyncHandler = <T>(
+    requestHandler: (req: Request, res: Response, next: NextFunction) => Promise<T>
 ) => {
     return (req: Request, res: Response, next: NextFunction) => {
         Promise.resolve(requestHandler(req, res, next)).catch(next);
