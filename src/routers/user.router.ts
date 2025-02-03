a051b1876a0abf2e0ce81a9b54eadf6ab55cbc0a
Plycedes <abhaypratapyadav340@gmail.com>
2025-02-03 22:27:30 +0530

    Update user.router.ts

--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,12 +1,18 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware";
 import { upload } from "../middlewares/multer.middleware";
-import { registerUser, loginUser, logoutUser } from "../controllers/user.controller";
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+} from "../controllers/user.controller";
 
 const router: Router = Router();
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.post("/refresh-token", refreshAccessToken);
 
 router.post("/logout", verifyJWT, logoutUser);
 export default router;
