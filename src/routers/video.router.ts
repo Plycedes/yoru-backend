f4c17f3bee0b04f4ae8e160668cee0e44ec1af3e
Plycedes <abhaypratapyadav340@gmail.com>
2025-02-08 21:51:46 +0530

    Update video.router.ts

--- src/routers/video.router.ts
+++ src/routers/video.router.ts
@@ -1,7 +1,18 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware";
 import { upload } from "../middlewares/multer.middleware";
+import { createVideo } from "../controllers/video.controller";
 
 const router: Router = Router();
+
+router.post(
+    "/create-video",
+    verifyJWT,
+    upload.fields([
+        { name: "thumbnail", maxCount: 1 },
+        { name: "video", maxCount: 1 },
+    ]),
+    createVideo
+);
 
 export default router;
