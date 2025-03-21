f631089e48587e3fb2d4d85d91952cbbb7db0eb5
Plycedes <abhaypratapyadav340@gmail.com>
2025-03-21 18:54:24 +0530

    Follow requests
    Unfollow, Followers, Following

--- src/controllers/follow.controller.ts
+++ src/controllers/follow.controller.ts
@@ -33,3 +33,42 @@ export const follow = asyncHandler(async (req: CustomRequest<FollowBody>, res: R
         .status(200)
         .json(new ApiResponse(200, followedUser, "Followed the user successfully"));
 });
+
+export const unfollow = asyncHandler(async (req: CustomRequest<FollowBody>, res: Response) => {
+    const { followed } = req.body;
+    const follower = req.user?._id;
+
+    if (!followed || !follower) {
+        throw new ApiError(400, "No field can be empty");
+    }
+
+    const result = await Follow.deleteOne({ followed, follower });
+
+    if (result.deletedCount > 0) {
+        return res.status(200).json(new ApiResponse(200, {}, "Unfollowed the user successfully"));
+    } else {
+        return res
+            .status(200)
+            .json(new ApiResponse(200, {}, "User already unfollowed or was never followed"));
+    }
+});
+
+export const followers = asyncHandler(async (req: CustomRequest, res: Response) => {
+    const followed = req.user?._id;
+
+    const followers = await Follow.find({ followed });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, followers.length, "Fetched the followers successfully"));
+});
+
+export const following = asyncHandler(async (req: CustomRequest, res: Response) => {
+    const follower = req.user?._id;
+
+    const following = await Follow.find({ follower });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, following.length, "Fetched the following successfully"));
+});
--- src/routers/follow.router.ts
+++ src/routers/follow.router.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware";
-import { follow } from "../controllers/follow.controller";
+import { follow, unfollow, followers, following } from "../controllers/follow.controller";
 
 const router: Router = Router();
 
 router.route("/follow").post(verifyJWT, follow);
+router.route("/unfollow").post(verifyJWT, unfollow);
+
+router.route("/followers").get(verifyJWT, followers);
+router.route("/following").get(verifyJWT, following);
 
 export default router;
